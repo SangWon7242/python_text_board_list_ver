201a68c874f8df3c2150f0f47444e761474c2b83
SangWon7242 <gotkdqja@gmail.com>
2022-08-06 11:48:05 +0900

    read 구현

--- Exam0806.py
+++ Exam0806.py
@@ -1,17 +1,14 @@
 # 리스트를 이용해 데이터를 관리하는 프로그램을 만들어보겠습니다.
-# 입력값에 add를 입력하면 아래처럼 나오게 해주세요.
+# 입력값에 read를 입력하면 아래처럼 나오게 해주세요.
 
 '''
-(입출력 예시)
+명령어를 입력해주세요: read
 
-명령어를 입력해주세요 : add
+현재 저장되어 있는 값 : [저장된 값들]
 
-저장할 값을 입력해주세요 : hello
-hello 이/가 저장되었습니다.
-(실제 리스트에 추가 한 데이터가 저장 되어야 합니다.)
-
-만약 add, exit 말고 다른 값을 입력하면 '올바른 명령어를 입력해주세요' 출력
+만약 add, read, exit 말고 다른 값을 입력하면 '올바른 명령어를 입력해주세요 출력'
 '''
+
 data_store = []
 
 while True:
@@ -28,6 +25,13 @@ while True:
         input_data = input("저장할 값을 입력해주세요 : ")
         print("{} 이/가 저장되었습니다.".format(input_data))
         data_store.append(input_data)
+
+    elif cmd == "read":
+        if len(data_store) == 0:
+            print("현재 저장되어 있는 데이터가 없습니다.")
+            continue
+
+        print("현재 저장되어 있는 값 : {}".format(data_store))
 
     elif cmd == "exit":
         print("프로그램이 종료 되었습니다.")
