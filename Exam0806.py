230c28d12eb61381198b737f4f61c2f40c679f77
SangWon7242 <gotkdqja@gmail.com>
2022-08-07 11:16:06 +0900

    기존 로직을 함수 단위로 분리

--- Exam0806.py
+++ Exam0806.py
@@ -1,77 +1,80 @@
-# 리스트를 이용해 데이터를 관리하는 프로그램을 만들어보겠습니다.
-# input() 함수를 이용해 입력값을 받습니다. 입력값을 받을 때는 '명령어를 입력해주세요 : '라고 출력문이 나오도록 합니다.
-# 입력값에 delete를 입력하면 아래와 같이 출력되도록 해주세요.
+def help_list():
+    print("add : 데이터 추가")
+    print("read : 데이터 조회")
+    print("update : 데이터 수정")
+    print("delete : 데이터 삭제")
 
-'''
-명령어를 입력해주세요 : delete
 
-['hello', 'bye']
-삭제 대상을 선택하세요 : 0
-hello 값이 삭제되었습니다.
+def do_add():
+    input_data = input("저장할 값을 입력해주세요 : ")
+    print("{} 이/가 저장되었습니다.".format(input_data))
+    data_store.append(input_data)
 
-* 만약1 해당하지 않는 인덱스를 입력하면 '올바른 값을 입력해주세요' 출력하고 다시 진행
 
-* 만약2 add, read, update, delete, exit 말고 다른 값을 입력하면 '올바른 명령어를 입력해주세요 출력'
-'''
+def show_list():
+    if len(data_store) == 0:
+        print("현재 저장되어 있는 데이터가 없습니다.")
+        return
+
+    print("현재 저장되어 있는 값 : {}".format(data_store))
+
+
+def do_update():
+    last_data_index = len(data_store) - 1
+
+    if len(data_store) == 0:
+        print("현재 저장되어 있는 데이터가 없습니다.")
+        return
+
+    print(data_store)
+    input_index = int(input("수정 대상을 선택하세요 : "))
+
+    if input_index > last_data_index:
+        print("{} 이하로 입력해주세요.".format(last_data_index))
+        return
+
+    input_update_data = input("어떤 값으로 수정할까요 : ")
+    data_store[input_index] = input_update_data
+    print("{} 로 값이 수정되었습니다.".format(input_update_data))
+
+
+def do_delete():
+    last_data_index = len(data_store) - 1
+
+    if len(data_store) == 0:
+        print("현재 저장되어 있는 데이터가 없습니다.")
+        return
+
+    print(data_store)
+    input_index = int(input("삭제 대상을 선택하세요 : "))
+
+    if input_index > last_data_index:
+        print("{} 이하로 입력해주세요.".format(last_data_index))
+        return
+
+    print("{} 값이 삭제되었습니다.".format(data_store[input_index]))
+    del data_store[input_index]
+
 
 data_store = []
 
 while True:
     cmd = input("명령어를 입력해주세요 : ")
-    # print("입력받은 명령어 : {}".format(cmd))
 
     if cmd == "help":
-        print("add : 데이터 추가")
-        print("read : 데이터 조회")
-        print("update : 데이터 수정")
-        print("delete : 데이터 삭제")
+        help_list()
 
     elif cmd == "add":
-        input_data = input("저장할 값을 입력해주세요 : ")
-        print("{} 이/가 저장되었습니다.".format(input_data))
-        data_store.append(input_data)
+        do_add()
 
     elif cmd == "read":
-        if len(data_store) == 0:
-            print("현재 저장되어 있는 데이터가 없습니다.")
-            continue
-
-        print("현재 저장되어 있는 값 : {}".format(data_store))
+        show_list()
 
     elif cmd == "update":
-        last_data_index = len(data_store) - 1
-
-        if len(data_store) == 0:
-            print("현재 저장되어 있는 데이터가 없습니다.")
-            continue
-
-        print(data_store)
-        input_index = int(input("수정 대상을 선택하세요 : "))
-
-        if input_index > last_data_index:
-            print("{} 이하로 입력해주세요.".format(last_data_index))
-            continue
-
-        input_update_data = input("어떤 값으로 수정할까요 : ")
-        data_store[input_index] = input_update_data
-        print("{} 로 값이 수정되었습니다.".format(input_update_data))
+        do_update()
 
     elif cmd == "delete":
-        last_data_index = len(data_store) - 1
-
-        if len(data_store) == 0:
-            print("현재 저장되어 있는 데이터가 없습니다.")
-            continue
-
-        print(data_store)
-        input_index = int(input("삭제 대상을 선택하세요 : "))
-
-        if input_index > last_data_index:
-            print("{} 이하로 입력해주세요.".format(last_data_index))
-            continue
-
-        print("{} 값이 삭제되었습니다.".format(data_store[input_index]))
-        del data_store[input_index]
+        do_delete()
 
     elif cmd == "exit":
         print("프로그램이 종료 되었습니다.")
